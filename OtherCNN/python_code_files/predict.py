a28aa140213559c3e2f51206be32125a8e5011a9
Rahul Yedida <yrahul3910@gmail.com>
2018-10-05 16:30:48 +0530

    Updated prediction code

--- OtherCNN/python_code_files/predict.py
+++ OtherCNN/python_code_files/predict.py
@@ -1,16 +1,21 @@
 from keras.models import load_model
 from astropy.io.fits import getdata, PrimaryHDU
 import keras.backend as K
-import pickle
 import numpy as np
 
 model = load_model('checkpoint-60.h5')
 img = getdata('BadImage.fits')
+
+# Preprocess
+img_max = np.max(img)
+img /= img_max
 img = np.array([img.reshape(*(img.shape), 1)])
 
 prediction = model.predict(img)[0]
+prediction = prediction.reshape(prediction.shape[0], prediction.shape[1])
+prediction *= img_max
+
 K.clear_session()
 print(prediction.shape)
-#hdu = PrimaryHDU(prediction[0])
-with open('prediction.h5', 'wb') as f:
-	pickle.dump(prediction, f)
+hdu = PrimaryHDU(prediction)
+hdu.writeto('prediction.fits')
